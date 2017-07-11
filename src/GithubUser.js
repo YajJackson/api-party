4d5ff91bcd402df53c953f6e8c164487360be9c4
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-07-11 11:29:14 -0400

    add GithubUser component

---
+++ src/GithubUser.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class GithubUser extends Component { //why not make this an anonymous function?
+    render() {
+        return <h1>Github User</h1>
+    }
+}
+
+export default GithubUser
